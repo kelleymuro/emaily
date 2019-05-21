ed784821a9ae450ae8f733145880388cca0ae59a
Kelley Muro <kelleymuro@gmail.com>
2019-05-20 19:28:48 -0700

    added billing and client side app!

--- services/passport.js
+++ services/passport.js
@@ -22,17 +22,16 @@ passport.use (new GoogleStrategy( {
    callbackURL: '/auth/google/callback',
    proxy: true
  }, 
- (accessToken, refreshToken, profile, done) => {
-   User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        // We already have a record with the given profile id
-        done(null, existingUser);
-      } else {
-        // We dont have a user record, so lets create one!
-        new User({ googleId: profile.id, name: profile.displayName }).save()
-          .then(user => done(null, user));
-      }
-    }) 
- })
+ async (accessToken, refreshToken, profile, done) => {
+   const existingUser = await User.findOne({ googleId: profile.id }) 
+    if (existingUser) {
+       
+    done(null, existingUser);
+
+    } 
+       
+    const user = await new User({ googleId: profile.id, name: profile.displayName }).save()
+    done(null, user);   
+  }
+ )
 );
